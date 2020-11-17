08b7544dc3419bc8f1ee2a4e33ae45ff2ee0d42e
DanielaMorariu1990 <57063213+DanielaMorariu1990@users.noreply.github.com>
2020-11-17 19:43:51 +0100

    entrance as initial state in tranistion matrix

--- transition_matrix.py
+++ transition_matrix.py
@@ -11,18 +11,33 @@ import datetime
 
 
 def missing_checkout(data):
+    """fixes data quuality issue: last cvustomers of the day
+           are missing from checkout """
+
+    data["timestamp"] = pd.to_datetime(data["timestamp"])
+    data.set_index("timestamp", inplace=True)
+
     checkout = set(data[data["location"] == "checkout"]
                    ["customer_no"].unique())
     all_c = set(data["customer_no"].unique())
     diff = all_c.difference(checkout)
+
+    current_date = data.index.date[0]
+    new_time_stemp = f"{current_date} 22:00:00"
+
+    data.reset_index(inplace=True)
+
     for cust in diff:
-        data = data.append({"timestamp": "2019-09-02 22:00:00", "customer_no": cust,
+        data = data.append({"timestamp": pd.to_datetime(new_time_stemp), "customer_no": cust,
                             "location": "checkout"}, ignore_index=True)
 
     return data
 
+# each customer starts from initial state entrace. we need to insert this state in the data
+
 
 def inserting_initial_state(data):
+
     min_datetime = data.groupby("customer_no")[
         "timestamp"].first().reset_index()
     one_minute = datetime.timedelta(minutes=1)
@@ -35,15 +50,13 @@ def inserting_initial_state(data):
 
 
 # make customer name unique
-
-
 def cust_no_name(data, weekday):
     data["customer_no"] = data["customer_no"].apply(
         lambda x: str(x)+"_" + weekday)
     return data
 
 
-##read in data
+# read in data for each weekdays
 monday = pd.read_csv("./data/monday.csv", header=0, sep=";", parse_dates=True)
 monday = missing_checkout(monday)
 monday["weekday"] = "monday"
@@ -76,20 +89,22 @@ cust_no_name(friday, "friday")
 
 # concatenate each weekday in one data frame
 data = pd.concat([monday, tuesday, wednesday, thursday, friday])
-data["timestamp"] = pd.to_datetime(data["timestamp"])
-data.set_index("timestamp")
 
+# inserting initial state
 data = inserting_initial_state(data)
+
+# we need to reset index such that we sort correctly
+data.reset_index(inplace=True)
+del data["index"]
 
 # values need to be sorted inplace, such that we have
 # correct transitions
 # calculate next location (transition from one section to the other)
 data.sort_values(["customer_no", "timestamp"], inplace=True)
-print(data.head(10))
+
 
 data["location_next"] = data["location"].shift(-1)
-print("next location")
-print(data.head(10))
+
 # checkout will only migrate to checkout!
 data.loc[(data.location == 'checkout'), 'location_next'] = 'checkout'
 
