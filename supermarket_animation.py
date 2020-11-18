68f082f995e615093b3b7ecde2ad7f094847395e
DanielaMorariu1990 <57063213+DanielaMorariu1990@users.noreply.github.com>
2020-11-18 10:10:42 +0100

    supermarket animation changes

--- supermarket_animation.py
+++ supermarket_animation.py
@@ -4,3 +4,18 @@ import numpy as np
 im = Image.open('images/supermarket.png')
 market = np.array(im)
 print(market.shape, market.dtype)
+
+im2 = Image.open('images/tiles.png')
+tiles = np.array(im2)
+print(tiles.shape, tiles.dtype)
+
+x = 4 * 32   # 5th column starting from 0
+y = 1 * 32   # 2nd row
+apple = tiles[y:y + 32, x:x + 32]
+
+tx = 13 * 32
+ty = 2 * 32
+market[ty:ty+32, tx:tx+32] = apple
+
+im = Image.fromarray(market)
+im.save('./images/supermarket_filled.png')
