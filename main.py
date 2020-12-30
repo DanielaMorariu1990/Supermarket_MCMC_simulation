b88eaccae1a55b4624736058c8d17e3bb999abae
DanielaMorariu1990 <57063213+DanielaMorariu1990@users.noreply.github.com>
2020-12-30 10:40:20 +0100

    make customers move between minutes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import datetime
+from random import random
 
 import numpy as np
 import pandas as pd
@@ -28,41 +29,60 @@ pacman2 = tiles[3 * 32:4 * 32, 1 * 32:2 * 32]
 ghost = tiles[7 * 32:8 * 32, 1 * 32:2 * 32]
 
 
-if __name__ == '__main__':
+def move_between_minutes(market, supermarket, frame):
+    ''' move the customers between the minutes'''
+    market.draw(frame)
+    supermarket.remove_existing_customers()
+    supermarket.move_customers()
+    supermarket.print_customers()
+    supermarket.draw_customers(frame)
+    time.sleep(0.1)
+
+
+def go_to_next_minute(market, supermarket, frame, adding_prob=0):
+    '''
+    simulate next minute
+    '''
+    market.draw(frame)
+    if random() < adding_prob:
+        supermarket.add_new_customers()
+    supermarket.remove_existing_customers()
+    supermarket.print_customers()
+
+    supermarket.draw_customers(frame)
+    supermarket.next_minute()
+    time.sleep(0.1)
+    return supermarket
+
+
+def simulate_n_customers(n, ghost, transition_matrix):
+    '''
+    Simulate n customers that start together in the store.
+    No new customers are added over time.
+    The simulation ends once all are checked out.
+    '''
     background = np.zeros((700, 1000, 3), np.uint8)
-    penny = Supermarket()
-    marketMap = SupermarketMap(MARKET, tiles)
-    penny.get_time()
-    penny.add_new_customers(1, 0, marketMap,
-                            ghost, entrance[0], entrance[1])
-    my_suffix = 1
-    while True:
+    market = SupermarketMap(MARKET, tiles)
+    supermarket = Supermarket(market=market)
+    id_suffix = 0
+    for _ in range(n):
+        supermarket.add_new_customers(
+            stop=1, id_suffix=id_suffix,  terrain_map=market, image=ghost, x=entrance[0], y=entrance[1])
+    frame = background.copy()
 
-        frame = background.copy()
-        marketMap.draw(frame)
-
-        for i in range(len(penny.customers)):
-            penny.customers[i].draw(frame)
-            print(penny.customers[i])
-
-        time.sleep(2)
-
-        penny.next_minute()
-        print(penny.print_customers())
-
-        # we need to iterate in oredr to call .move()
-
-        penny.remove_existing_customers()
-        # penny.add_new_customers(1, my_suffix, marketMap,
-        #                         pacman, entrance[0], entrance[1])
-
-        # penny.add_new_customers(1, my_suffix, marketMap,
-        #                         ghost, entrance[0], entrance[1])
-        # my_suffix += 1
-
-        print(penny.print_customers())
-        cv2.imshow('frame', frame)
+    while len(supermarket.customers) > 0:
+        if supermarket.to_move:
+            move_between_minutes(market, supermarket, frame)
+        else:
+            go_to_next_minute(market, supermarket, frame)
+        cv2.imshow("frame", frame)
 
         key = chr(cv2.waitKey(1) & 0xFF)
         if key == 'q':
             break
+    cv2.destroyAllWindows()
+    market.write_image("./graphics/supermarket.png")
+    id_suffix += 1
+
+
+simulate_n_customers(n=10, ghost=ghost, transition_matrix=transition_matrix)
